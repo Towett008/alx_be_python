0eeb05840992fd1d1c0499ced9010813e9e2315b
Cynthia Chepkorir <towettcynthia041@gmail.com>
2025-09-15 19:33:17 +0300

    Add simpole_interest.py to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+principal = 1000 #Initial investment
+rate = 0.05  #Annual intrest rate (5%)
+time = 3  #Time in years
+
+
+# Simple intrest formula
+interest = principal * rate * time
+
+
+# Print the result
+print(f"The simple intrest is: {interest}")

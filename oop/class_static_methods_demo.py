4969f928b37a63ca6ac5b9dd8d9e0cfd87f599d8
Cynthia Chepkorir <towettcynthia041@gmail.com>
2025-10-12 20:12:18 +0300

    Add calculator class with static and class methods demonstation

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,14 @@
+class Calculator:
+    #Class Attribute
+    calculation_type = "Arithmetic Operations"
+
+    #Static method does not use self or cls
+    @staticmethod
+    def add(a, b):
+        return a + b
+    
+    # Class method - accesses class-level data via cls
+    @classmethod
+    def multiply (cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b 
